7f40b943215d8aaf273f6bba764b459ffae746f4
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2026-05-22 09:06:01 -0400

    Do Simple Serve

--- ClaseViernes22May/app.js
+++ ClaseViernes22May/app.js
@@ -1,9 +1,18 @@
 import {addText , getContent} from "./fileService.js";
+import http from "http"
 
-async function main(){
-    await addText("Ernesto");
-    const fileContent = await getContent();
-    console.log(fileContent);
-}
+const PORT = 3000;
 
-main();
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    res.end(`
+        <h1>Hola soy Ernesto desde el servidor</h1>
+        <p> Este HTML fue enviado desde local con Node.js</p>
+    `)
+});
+
+server.listen(PORT, () => {
+    console.log("Servidor disponibel en http://localhost:" + PORT + "/");
+});
